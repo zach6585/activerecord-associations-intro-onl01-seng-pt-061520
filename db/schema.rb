e8a5f06dfc81ae5e31bb3a92e94bf718f53a5052
zach6585 <zmines@wesleyan.edu>
2020-08-28 17:22:15 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,10 @@ ActiveRecord::Schema.define(version: 4) do
     t.string "name"
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
