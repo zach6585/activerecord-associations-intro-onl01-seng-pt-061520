4eec49be100182b53797c490888fa68bbad02842
zach6585 <zmines@wesleyan.edu>
2020-08-28 16:56:09 -0400

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,7 +1,7 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
-  def change
+  def change 
     create_table :genres do |t|
-      t.string :name
-    end
-  end
+      t.string :name 
+    end 
+  end 
 end
